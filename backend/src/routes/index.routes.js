1a44f3f1d4e8c4e83d4eafe4f19316c70e650f34
Andrés Felipe Torres Castro <andres.torres1901@alumnos.ubiobio.cl>
2024-10-07 17:00:29 -0300

    feat: agregar controladores, rutas, esquemas y servicios de autenticación y usuarios
    - Añadidos controladores: auth.controller.js, user.controller.js.
    - Añadidos middlewares de autenticación y autorización.
    - Creado esquema de roles y usuarios: auth.schema.js, user.schema.js.
    - Añadidas rutas para autenticación y usuarios: auth.routes.js, user.routes.js, index.routes.js.
    - Añadido servicio de autenticación y usuarios: auth.service.js, user.service.js.
    - Estructura del proyecto actualizada con modelos y esquemas relacionados.

---
+++ backend/src/routes/index.routes.js
@@ -0,0 +1,23 @@
+"use strict";
+// Importa el modulo 'express' para crear las rutas
+import { Router } from "express";
+
+/** Enrutador de usuarios  */
+import userRoutes from "./user.routes.js";
+
+/** Enrutador de autenticación */
+import authRoutes from "./auth.routes.js";
+
+/** Middleware de autenticación */
+import authenticationMiddleware from "../middlewares/authentication.middleware.js";
+
+/** Instancia del enrutador */
+const router = Router();
+// Define las rutas para los usuarios /api/usuarios
+router.use("/users", authenticationMiddleware, userRoutes);
+// Define las rutas para la autenticación /api/auth
+router.use("/auth", authRoutes);
+// Define las rutas para la alimentación /alimentacion
+
+// Exporta el enrutador
+export default router;
